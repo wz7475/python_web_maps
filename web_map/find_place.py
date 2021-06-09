590816de20ac9c0b880b6395f1e73addfe9c6981
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-06-09 20:21:11 +0200

    1

---
+++ web_map/find_place.py
@@ -0,0 +1,13 @@
+import webbrowser
+import folium
+from geopy.geocoders import ArcGIS
+nom = ArcGIS()
+# with open("web_map\\data\\find.txt") as f:
+#     address = f.read()
+print("Enter name of certrain city or country:")
+address = input()
+m=nom.geocode(address)
+map = folium.Map(location=[m.latitude, m.longitude], zoom_start=8)
+folium.Marker(location=[m.latitude, m.longitude], color="green").add_to(map)
+map.save("web_map\\html\\find_place.html")
+webbrowser.open("web_map\\html\\find_place.html")
